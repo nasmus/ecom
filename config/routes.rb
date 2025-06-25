54a9144ed98c062f65697b30f3eb838c4b56d2b6
nasmus <nasmus.shahadat@gmail.com>
2025-06-25 12:49:13 +0600

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
+  devise_for :admins
+
+  root to: "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
+
+
+end
